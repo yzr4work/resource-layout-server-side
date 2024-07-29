4d46af698ff19ce48ed942d7be9716d4323d044f
yzr <yanzongrui@gmail.com>
2024-07-29 09:50:55 +0800

    修改依赖mybatis-boot-starter版本

--- resource-layout-background/src/main/java/com/yzr/resource/layout/background/controller/FirstController.java
+++ resource-layout-background/src/main/java/com/yzr/resource/layout/background/controller/FirstController.java
@@ -6,20 +6,21 @@ import com.yzr.resource.layout.background.controller.vo.first.FirstReqParam;
 import com.yzr.resource.layout.background.controller.vo.first.FirstResp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/first")
 public class FirstController {
-    private Logger logger = LoggerFactory.getLogger(FirstController.class);
+    private final Logger LOGGER = LoggerFactory.getLogger(FirstController.class);
     @PostMapping("/firstMethod")
     public RespWarp<FirstResp> firstMethod(@RequestBody ReqParamWarp<FirstReqParam> reqParamWarp){
-        logger.info("firstMethod rev param : {}", reqParamWarp.getParam().toString());
+        LOGGER.info("firstMethod rev param : {}", reqParamWarp.getParam().toString());
         FirstResp resp = new FirstResp();
         resp.setMsg("hello world");
         return RespWarp.SUCCESS(resp);
+    }
+    @GetMapping("/index")
+    public String index(){
+        return "index2";
     }
 }
